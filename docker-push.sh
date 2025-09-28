926d2011694aee45013637f58f67e706fffa9aaf
labcabrera <lab.cabrera@gmail.com>
2025-09-28 08:39:50 +0200

    Renamed project.

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t labcabrera/rmu-fe-core:latest .
+docker build -t labcabrera/rmu-mfe-core:latest .
 
-docker push labcabrera/rmu-fe-core:latest
+docker push labcabrera/rmu-mfe-core:latest
